754fcbf6b817496e2617366e0f83611723b54e03
Prashan070 <prashansrivastava77@gmail.com>
2024-11-04 02:50:05 +0530

    Update 1207-unique-number-of-occurrences.java

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         HashMap<Integer, Integer> freqMap = new HashMap<>();
-
+//Look into this and do again "hmap.put(i,hmap.getOrDefault(i,0)+1);"
         for (int num : arr) {
             if (freqMap.containsKey(num)) {
                 freqMap.put(num, freqMap.get(num) + 1);
